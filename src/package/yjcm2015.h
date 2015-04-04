96dca79b66fbd5e9742725c0c05ba1e2838a3414
Yanguam Siliagim <Rara@mogara.org>
2015-04-05 00:56:16 +0800

    yjcm2015 update

--- src/package/yjcm2015.h
+++ src/package/yjcm2015.h
@@ -4,6 +4,26 @@
 #include "package.h"
 #include "card.h"
 
+class FurongCard : public SkillCard
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE FurongCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void onEffect(const CardEffectStruct &effect) const;
+};
+
+class YjYanyuCard : public SkillCard
+{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE YjYanyuCard();
+    virtual void onUse(Room *room, const CardUseStruct &card_use) const;
+};
+
 class YJCM2015Package : public Package
 {
     Q_OBJECT
