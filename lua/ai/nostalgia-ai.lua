5b452f15e9230e00742cb230fa75839018835cbf
tanANY <zyun7799@163.com>
2015-04-21 21:19:40 +0800

    fix 蛊惑

--- lua/ai/nostalgia-ai.lua
+++ lua/ai/nostalgia-ai.lua
@@ -926,7 +926,9 @@ sgs.ai_skill_use_func.NosGuhuoCard = function(card, use, self)
 	nosguhuocard:setSkillName("nosguhuo")
 	if nosguhuocard:getTypeId() == sgs.Card_TypeBasic then
 		self:useBasicCard(nosguhuocard, use)
-		if not use.isDummy and use.card and use.card:isKindOf("Slash") and (not use.to or use.to:isEmpty()) then return end
+		if use.card:isKindOf("Slash") then
+			if not use.to or use.to:isEmpty() then return end
+		end
 	else assert(nosguhuocard)
 		self:useTrickCard(nosguhuocard, use)
 	end
