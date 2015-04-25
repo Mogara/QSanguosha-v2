4e2f2cd8528a2f2e08843f27e3784a12505a5c7e
popkc <popkcer@gmail.com>
2015-04-25 19:58:19 +0800

    修改默认列表服务器

--- src/core/defines.h
+++ src/core/defines.h
@@ -1,7 +1,7 @@
-#ifndef DEFINES_H
+﻿#ifndef DEFINES_H
 #define DEFINES_H
 
-#define SERVERLIST_URL_DEFAULTGET "http://127.0.0.1:80/storage/fairsgs/fairsgs/"
+#define SERVERLIST_URL_DEFAULTGET "http://fairsgs-fairsgs.stor.sinaapp.com/"
 #define SERVERLIST_URL_DEFAULTREG "http://fairsgs.sinaapp.com/reg.php"
 #define SERVERLIST_VERSION_SERVERLIST 1
 
