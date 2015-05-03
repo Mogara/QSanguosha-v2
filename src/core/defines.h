a9a01389a4c247de63b2fc79109bd366bff3a914
popkc <popkcer@gmail.com>
2015-05-03 08:10:41 +0800

    高亮显示官方服务器，显示扩展包
    调整栏目大小

--- src/core/defines.h
+++ src/core/defines.h
@@ -4,6 +4,7 @@
 #define SERVERLIST_URL_DEFAULTGET "http://fairsgs-fairsgs.stor.sinaapp.com/"
 #define SERVERLIST_URL_DEFAULTREG "http://fairsgs.sinaapp.com/reg.php"
 #define SERVERLIST_VERSION_SERVERLIST 1
+#define SERVERLIST_OFFICIALSERVER "115.159.24.202"
 
 #endif // DEFINES_H
 
