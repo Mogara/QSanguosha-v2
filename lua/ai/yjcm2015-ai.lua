430d53f85f066425787b4853ef8411b6cfd97f4b
Yanguam Siliagim <Rara@mogara.org>
2015-07-04 18:40:17 +0800

    先让AI能用技能再说。。。

--- lua/ai/yjcm2015-ai.lua
+++ lua/ai/yjcm2015-ai.lua
@@ -46,13 +46,13 @@ sgs.ai_skill_invoke.jigong = function(self)
     for _, c in sgs.qlist(self.player:getHandcards()) do
         local x = nil
         if isCard("ArcheryAttack", c, self.player) then
-            x = sgs.cloneCard("ArcheryAttack")
+            x = sgs.Sanguosha:cloneCard("ArcheryAttack")
         elseif isCard("SavageAssault", c, self.player) then
-            x = sgs.cloneCard("SavageAssault")
+            x = sgs.Sanguosha:cloneCard("SavageAssault")
         else continue end
         
         local du = { isDummy = true }
-        self.useTrickCard(x, du)
+        self:useTrickCard(x, du)
         if (du.card) then return true end
     end
     
@@ -102,13 +102,12 @@ sgs.ai_skill_playerchosen.shifei = function(self, targets)
 end
 
 
-zhanjue_skill = {}
-zhanjue_skill.name = "zhanjue"
+zhanjue_skill = {name = "zhanjue"}
 table.insert(sgs.ai_skills, zhanjue_skill)
 zhanjue_skill.getTurnUseCard = function(self)
     if (self.player.getMark("zhanjuedraw") >= 2) then return nil end
     
-    if (self.player.isKongcheng()) then return nil end
+    if (self.player:isKongcheng()) then return nil end
     
     local duel = sgs.Sanguosha:cloneCard("duel", sgs.Card_SuitToBeDecided, -1)
     duel:addSubcards(self.player:getHandcards())
@@ -122,4 +121,51 @@ end
 
 -- zhenshan buhui!!!
 
+
+yanzhu_skill = {name = "yanzhu"}
+table.insert(sgs.ai_skills, yanzhu_skill)
+yanzhu_skill.getTurnUseCard = function(self) 
+
+    if (self.player:hasUsed("YanzhuCard")) then return nil end
+    
+    return sgs.Card_Parse("@YanzhuCard=.")
+
+end
+
+
+sgs.ai_skill_use_func.YanzhuCard = function(card, use, self)
+
+    if (#self.enemies == 0) then return nil end
+
+    self:sort(self.enemies, "threat")
+    for _, p in ipairs(self.enemies) do
+        if not p:isNude() then
+            use.card = card
+            if (use.to) then use.to:append(p) end
+            -- use.from = self.player
+            return 
+        end
+    end
+
+    return nil
+end
+
+sgs.ai_use_priority.YanzhuCard = sgs.ai_use_priority.Dismantlement - 0.1
+
+sgs.ai_skill_discard.yanzhu = function(self, _, __, optional)
+    if not optional then return self:askForDiscard("dummyreason", 1, 1, false, true) end
+    
+    if self:needToThrowArmor() and self.player:getArmor() and not self.player:isJilei(self.player:getArmor()) then return self.player:getArmor():getEffectiveId() end
+    
+    if self.player:getTreasure() then
+        if (self.player:getCardCount() == 1) then return self.player:getTreasure():getEffectiveId()
+        elseif not self.player:isKongcheng() then return self:askForDiscard("dummyreason", 1, 1, false, false) end
+    end
+    
+    if self.player:getEquips():length() > 2 and not self.player:isKongcheng() then return self:askForDiscard("dummyreason", 1, 1, false, false) end
+    
+    if self.player:getEquips():length() == 1 then return {} end
+    
+     return self:askForDiscard("dummyreason", 1, 1, false, true)
+end
 
